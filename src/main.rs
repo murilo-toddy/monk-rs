503db986d84bca08b944c7c0756deb0ba24c1dfe
murilo-toddy <murilo.todao@gmail.com>
2024-06-04 09:44:07 -0300

    feat: implement simple cli for running scripts from files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+use std::fs;
 use std::io;
+use std::env;
+use std::io::Write;
 
 pub mod token;
 pub mod lexer;
@@ -11,12 +14,38 @@ pub mod environment;
 pub mod builtins;
 
 fn main() -> io::Result<()> {
-    let input = io::stdin();
-    let output = io::stdout();
+    let args: Vec<String> = env::args().collect();
 
-    let mut reader = input.lock();
+    let output = io::stdout();
     let mut writer = output.lock();
 
-    repl::start(&mut reader, &mut writer)?;
+    if args.len() == 2 {
+        let input = fs::read_to_string(&args[1])
+            .expect("Should be able to read file");
+
+        let env = environment::Environment::new();
+        let mut evaluator = evaluator::Evaluator::new(env);
+        let lexer = lexer::Lexer::new(input.as_bytes());
+        let mut parser = parser::Parser::new(lexer);
+        let program = parser.parse();
+        if !parser.get_errors().is_empty() {
+            for error in parser.get_errors() {
+                writer.write_all(format!("{}", error).as_bytes())?;
+            }
+            writer.flush()?;
+        }
+        evaluator.evaluate_program(program);
+    }
+
+    else if args.len() == 1 {
+        let input = io::stdin();
+        let mut reader = input.lock();
+        repl::start(&mut reader, &mut writer)?;
+    }
+
+    else {
+        writer.write_all("Usage: cargo run -- <filename>.mk\n".as_bytes())?;
+        writer.flush()?;
+    }
     Ok(())
 }
